644b328087d2529abf48d3f3b40fb689db7c7269
Mark Gonzalez <mgon025@csu.fullerton.edu>
2022-04-15 17:09:51 -0700

    Added player animation and more sprites

--- player/Player.gd
+++ player/Player.gd
@@ -1,27 +1,34 @@
 extends KinematicBody2D
 
 var speed: int = 100
-var velocity: Vector2 = Vector2(speed, 0.0)
-onready var sprite: Sprite = get_node("Sprite")
+var force: Vector2 = Vector2(speed, 0.0)
 
 func _physics_process(_delta):
+	var curr_vel: Vector2 = movement()
+	animation(curr_vel)
+
+func animation(velocity: Vector2):
+	# stop animation when velocity = (0,0)
+	$AnimationTree.active = true if velocity else false
+
+	# adjust sprite rotation based on movement direction
+	if force.y < 0:
+		$Sprite.set_rotation(-PI/2)
+	elif force.x > 0:
+		$Sprite.set_rotation(0.0)
+	elif force.y > 0:
+		$Sprite.set_rotation(PI/2)
+	elif force.x < 0:
+		$Sprite.set_rotation(PI)
+
+func movement() -> Vector2:
 	if Input.is_action_just_pressed("ui_up"):
-		velocity = Vector2(0, -speed)
+		force = Vector2(0, -speed)
 	elif Input.is_action_just_pressed("ui_right"):
-		velocity = Vector2(speed, 0)
+		force = Vector2(speed, 0)
 	elif Input.is_action_just_pressed("ui_down"):
-		velocity = Vector2(0, speed)
+		force = Vector2(0, speed)
 	elif Input.is_action_just_pressed("ui_left"):
-		velocity = Vector2(-speed, 0)
-
-	move_and_slide(velocity)
-
-	# adjust sprite rotation based on move direction
-	if velocity.y < 0:
-		sprite.set_rotation(-PI/2)
-	elif velocity.x > 0:
-		sprite.set_rotation(0.0)
-	elif velocity.y > 0:
-		sprite.set_rotation(PI/2)
-	elif velocity.x < 0:
-		sprite.set_rotation(PI)
+		force = Vector2(-speed, 0)
+	
+	return move_and_slide(force)
