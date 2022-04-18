4d2afc3517a86cbff0fd3ab65672e6dbbdf78141
Mark Gonzalez <mgon025@csu.fullerton.edu>
2022-04-17 17:20:43 -0700

    Fixed format to comply with gdscript style

--- Test/Unit/test_player.gd
+++ Test/Unit/test_player.gd
@@ -1,6 +1,6 @@
 extends "res://addons/gut/test.gd"
 
-var Player = load("res://player/Player.gd")
+var Player = load("res://player/player.gd")
 var _player = null
 
 func before_each():
@@ -11,24 +11,25 @@ func after_each():
 
 func test_take_life():
 	_player.lives = 3
-	var result = _player.take_life(1)
+	_player.take_life(1)
 	
 	assert_eq(_player.lives, 2, "Lives should be 3 - 1")
 
 func test_take_over_life():
 	_player.lives = 3
-	var result = _player.take_life(4)
+	_player.take_life(4)
 	
 	assert_eq(_player.lives, 0, "Lives should not be less than 0")
 
 func test_add_life():
 	_player.lives = 2
-	var result = _player.take_life(-1)
+	_player.take_life(-1)
 	
 	assert_eq(_player.lives, 3, "Lives should be 2 + 1")
 
 func test_add_over_life():
 	_player.lives = 3
-	var result = _player.take_life(-1)
+	_player.take_life(-1)
 	
-	assert_eq(_player.lives, _player.max_lives, "Lives should not exceed " + str(_player.max_lives))
+	assert_eq(_player.lives, _player.MAX_LIVES, "Lives should not exceed "\
+			+ str(_player.MAX_LIVES))
