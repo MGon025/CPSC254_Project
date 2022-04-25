c0506328dab8e0b616ceacd0df099418927730b1
Shalom Yemane <syemane@csu.fullerton.edu>
2022-04-25 12:12:26 -0700

    Follow algorithm polished

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var speed: int = 100
 var force: Vector2 = Vector2(speed, 0.0)
-var current_direction: int = 1 # 0 = UP, 1 = RIGHT, 2 = DOWN, 3 = LEFT
+var current_direction: int = -1 # 0 = UP, 1 = RIGHT, 2 = DOWN, 3 = LEFT
 
 onready var player_body: KinematicBody2D = get_node("../Player")
 
@@ -16,28 +16,40 @@ func animation(velocity: Vector2):
 
 	# change row on spritesheet depending on direction of movement
 	if force.y < 0:
-		$Sprite.frame_coords.y = 2
+		$Sprite.frame_coords.y = 0
 	elif force.x > 0:
 		$Sprite.frame_coords.y = 1
 	elif force.y > 0:
-		$Sprite.frame_coords.y = 0
+		$Sprite.frame_coords.y = 2
 	elif force.x < 0:
 		$Sprite.frame_coords.y = 3
 
 func movement() -> Vector2:
 	var gap:int = 3
-			
-	if position.x + gap < player_body.position.x:
-		current_direction = 1
-	elif position.x - gap > player_body.position.x:
-		current_direction = 3
-	elif position.y + gap < player_body.position.y:
-		current_direction = 2
-	elif position.y - gap > player_body.position.y:
-		current_direction = 0
-			
-	if current_direction < 0:
-		current_direction += 4
+	
+	match current_direction:
+		0:
+			if position.y - gap <= player_body.position.y:
+				current_direction = -1
+		1:
+			if position.x + gap >= player_body.position.x:
+				current_direction = -1
+		2:
+			if position.y + gap >= player_body.position.y:
+				current_direction = -1
+		3:
+			if position.x - gap <= player_body.position.x:
+				current_direction = -1
+				
+	if current_direction == -1:
+		if position.x + gap < player_body.position.x:
+			current_direction = 1
+		elif position.x - gap > player_body.position.x:
+			current_direction = 3
+		elif position.y + gap < player_body.position.y:
+			current_direction = 2
+		elif position.y - gap > player_body.position.y:
+			current_direction = 0
 	
 	if current_direction % 4 == 0:
 		force = Vector2(0, -speed)
