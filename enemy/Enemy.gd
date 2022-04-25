755aaca43424b958c7c54fb7b72235fb6f675be8
Shalom Yemane <syemane@csu.fullerton.edu>
2022-04-25 12:20:13 -0700

    Comments

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -2,9 +2,9 @@ extends KinematicBody2D
 
 var speed: int = 100
 var force: Vector2 = Vector2(speed, 0.0)
-var current_direction: int = -1 # 0 = UP, 1 = RIGHT, 2 = DOWN, 3 = LEFT
+var current_direction: int = -1 # -1 = No Movement, 0 = UP, 1 = RIGHT, 2 = DOWN, 3 = LEFT
 
-onready var player_body: KinematicBody2D = get_node("../Player")
+onready var player_body: KinematicBody2D = get_node("../Player") # Player's sprite body
 
 func _physics_process(_delta):
 	var curr_vel: Vector2 = movement()
@@ -27,6 +27,7 @@ func animation(velocity: Vector2):
 func movement() -> Vector2:
 	var gap:int = 3
 	
+	# Check if current movement is no longer needed
 	match current_direction:
 		0:
 			if position.y - gap <= player_body.position.y:
@@ -41,6 +42,7 @@ func movement() -> Vector2:
 			if position.x - gap <= player_body.position.x:
 				current_direction = -1
 				
+	# Decide which direction to move, ONLY if not currently moving (current_direction == -1)
 	if current_direction == -1:
 		if position.x + gap < player_body.position.x:
 			current_direction = 1
@@ -51,13 +53,14 @@ func movement() -> Vector2:
 		elif position.y - gap > player_body.position.y:
 			current_direction = 0
 	
-	if current_direction % 4 == 0:
+	# Apply force according to desired direction.
+	if current_direction == 0:
 		force = Vector2(0, -speed)
-	elif current_direction % 4 == 1:
+	elif current_direction == 1:
 		force = Vector2(speed, 0)
-	elif current_direction % 4 == 2:
+	elif current_direction == 2:
 		force = Vector2(0, speed)
-	elif current_direction % 4 == 3:
+	elif current_direction == 3:
 		force = Vector2(-speed, 0)
 	
 	return move_and_slide(force)
