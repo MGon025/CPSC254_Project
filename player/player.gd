7818c2bae1512150036dea32e9514bd0bae47232
Mark Gonzalez <mgon025@csu.fullerton.edu>
2022-04-25 21:04:50 -0700

    author info

--- player/player.gd
+++ player/player.gd
@@ -14,6 +14,9 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see https://www.gnu.org/licenses/.
 
+# Mark Gonzalez
+# mgon025@csu.fullerton.edu
+
 
 extends KinematicBody2D
 
